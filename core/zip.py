e4b920a25c567c811900569b1e2b71f49fdd05a5
Ilya_Miflig <Ilya_Miflig@epam.com>
2021-02-12 09:58:21 +0200

    zip usage

--- core/zip.py
+++ core/zip.py
@@ -1,11 +1,18 @@
+import numpy as np
+
 numbers = [1, 2, 3]
 letters = ['a', 'b', 'c']
 zipped = zip(numbers, letters)
 
 print(type(zipped))
-print(list(zipped))
+l_zip = list(zipped)
+print(l_zip)
 
 # need to init zipped again
 zipped = zip(numbers, letters)
 for (n, l) in zipped:
     print(n, l)
+
+print('--------------------------')
+l_zip_2 = list(zip(*l_zip))
+print(l_zip_2)
